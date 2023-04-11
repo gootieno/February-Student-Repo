9da3f98bd524f674f0faca7a0610c2bebc5cba40
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-04-11 10:36:56 -0700

    formulating a response

--- Week8/Day2/solutions/02-formulate-a-response-in-http/server.js
+++ Week8/Day2/solutions/02-formulate-a-response-in-http/server.js
@@ -2,16 +2,25 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    const responseBody = 'Just some text';
+    const responseBody = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Hello World!</title>
+    </head>
+    <body>
+      <h1>Hello there!</h1>
+    </body>
+    </html>
+  `;
 
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain')
+    res.setHeader('Content-Type', 'text/html')
     return res.end(responseBody)
 })
 
 const port = 5000;
 
 server.listen(port, () => console.log('Server is listening on port', port));
-
-
-console.log('This will run')
