390600bec18cd6b05c89f6ecfe0e7e8a1e0250a3
Tanner Shaw <traumaaa@gmail.com>
2023-04-13 12:45:01 -0500

    test api endpoints solution

--- Week8/Day4/solutions/04-test-api-endpoints/fetch-requests.js
+++ Week8/Day4/solutions/04-test-api-endpoints/fetch-requests.js
@@ -11,8 +11,32 @@ Paste your code for fetch requests here once you finish each task.
 
 // Your code here
 
+fetch('/posts')
+  .then(res => res.json())
+  .then(resBody => console.log(resBody));
 
+(async function () {
+  const res = await fetch('/posts');
+  console.log(res.status);
+  console.log(res.headers.get('content-type'));
+  const deserializedBody = await res.json();
+  console.log(deserializedBody);
+})();
 
+fetch('/posts')
+  .then(async res => {
+    console.log(res.status);
+    console.log(res.headers.get('content-type'));
+    const deserializedBody = await res.json();
+    console.log(deserializedBody);
+
+  });
+
+const postsFetch = async () => {
+  const res = await fetch('/posts');
+  const jsonBody = await res.json();
+  console.log(jsonBody);
+}
 /* =============================== Phase 2 ================================ */
 /*
   Make a request with fetch request to POST /posts and print the response
@@ -20,3 +44,34 @@ Paste your code for fetch requests here once you finish each task.
 */
 
 // Your code here
+fetch('/posts', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    message: "New Post!"
+  })
+})
+  .then(res => res.json())
+  .then(resBody => console.log(resBody));
+
+
+(async function () {
+  const res = await fetch('/posts', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      message: "Hola!"
+    })
+  });
+
+  console.log(res.status);
+  console.log(res.headers.get('content-type'));
+  const serializedBody = await res.json();
+
+  console.log(JSON.parse(serializedBody));
+
+})();
