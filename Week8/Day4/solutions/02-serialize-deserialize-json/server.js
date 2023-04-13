310771df6484b1d3fde7d82f06e35b7521bed360
Tanner Shaw <traumaaa@gmail.com>
2023-04-13 12:24:59 -0500

    First 3 practice solutions

--- Week8/Day4/solutions/02-serialize-deserialize-json/server.js
+++ Week8/Day4/solutions/02-serialize-deserialize-json/server.js
@@ -10,29 +10,38 @@ const server = http.createServer((req, res) => {
 
   req.on("end", () => {
     // Parse the body of the request as JSON if Content-Type header is
-      // application/json
+    // application/json
     // Parse the body of the request as x-www-form-urlencoded if Content-Type
-      // header is x-www-form-urlencoded
+    // header is x-www-form-urlencoded
     if (reqBody) {
-      req.body = reqBody
-        .split("&")
-        .map((keyValuePair) => keyValuePair.split("="))
-        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
-        .map(([key, value]) => [key, decodeURIComponent(value)])
-        .reduce((acc, [key, value]) => {
-          acc[key] = value;
-          return acc;
-        }, {});
 
-      // Log the body of the request to the terminal
-      console.log(req.body);
+      if (req.headers['content-type'] === 'application/json') {
+        req.body = JSON.parse(reqBody);
+      }
+      if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+        req.body = reqBody
+          .split("&")
+          .map((keyValuePair) => keyValuePair.split("="))
+          .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+          .map(([key, value]) => [key, decodeURIComponent(value)])
+          .reduce((acc, [key, value]) => {
+            acc[key] = value;
+            return acc;
+          }, {});
+
+          // Log the body of the request to the terminal
+        }
+        console.log(req.body);
     }
-
     const resBody = {
       "Hello": "World!"
     };
 
     // Return the `resBody` object as JSON in the body of the response
+    res.body = JSON.stringify(resBody);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    return res.end(res.body);
   });
 });
 
--- Week8/Day4/solutions/03-creating-api-documentation/README.md
+++ Week8/Day4/solutions/03-creating-api-documentation/README.md
@@ -44,16 +44,32 @@ For example, to edit a specific post, the API endpoint could be
 `PATCH /posts/:postId`
 
 - Get all the posts
+`GET /posts`
 - Create a new post
+`POST /posts`
 - Edit a post
+`PATCH/PUT /posts/:postId`
 - Create a new user
+`POST /users`
 - Get the comments for a post
+`GET /posts/:postId/comments`
 - Create a comment for a post
+`POST /posts/:postId/comments`
 - Edit a comment for a post
+`PATCH/PUT /posts/:postId/comments/:commentId`
+`PATCH/PUT /comments/:commentId`
 - Delete a comment for a post
+`DELETE /posts/:postId/comments/:commentId`
+`DELETE /comments/:commentId`
 - Add a like for a post
+`POST /posts/:postId/likes`
 - Remove a like for a post
+`DELETE /posts/:postId/likes/:likeId`
+`DELETE /likes/:likeId`
 - Get all the posts of a user
+`GET /users/:userId/posts`
 - Submit a search on posts
+`GET/POST /search/posts`
+
 
 [starter]: https://github.com/appacademy/practice-for-week-08-creating-api-docs
