84d016ca3e22e8320360d89f1c1e628a1ab5a516
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-04-17 15:48:47 -0700

    es6 modules solution

--- Week9/Day1/solutions/02-es6-modules/assets/js/game.js
+++ Week9/Day1/solutions/02-es6-modules/assets/js/game.js
@@ -1,32 +1,34 @@
-class Game {
+import { getIndex } from "./utilities.js";
+import { mrPotatoHeadQuotes as mrQuotes } from "./quotes/mrPotatoHead.js";
+import mrsPotatoHeadQuotes  from "./quotes/mrsPotatoHead.js";
 
+export default class Game {
   start() {
-
-    document.getElementById('hello').addEventListener('click', () => {
+    document.getElementById("hello").addEventListener("click", () => {
       const index = getIndex();
-      const messageContainer = document.getElementById('message');
+      const messageContainer = document.getElementById("message");
       if (index === 1) {
-        messageContainer.innerText = mrPotatoHeadQuotes['hello'];
+        messageContainer.innerText = mrQuotes["hello"];
       } else {
-        messageContainer.innerText = mrsPotatoHeadQuotes['hello'];
+        messageContainer.innerText = mrsPotatoHeadQuotes["hello"];
       }
     });
 
-    document.getElementById('bye').addEventListener('click', () => {
+    document.getElementById("bye").addEventListener("click", () => {
       const index = getIndex();
-      const messageContainer = document.getElementById('message');
+      const messageContainer = document.getElementById("message");
       if (index === 1) {
-        messageContainer.innerText = mrPotatoHeadQuotes['bye'];
+        messageContainer.innerText = mrQuotes["bye"];
       } else {
-        messageContainer.innerText = mrsPotatoHeadQuotes['bye'];
+        messageContainer.innerText = mrsPotatoHeadQuotes["bye"];
       }
     });
 
-    document.getElementById('swap').addEventListener('click', () => {
+    document.getElementById("swap").addEventListener("click", () => {
       const index = getIndex();
-      const image = document.getElementById('image');
-      const messageContainer = document.getElementById('message');
-      const wrapper = document.getElementById('wrapper');
+      const image = document.getElementById("image");
+      const messageContainer = document.getElementById("message");
+      const wrapper = document.getElementById("wrapper");
       if (index === 1) {
         image.src = "./assets/images/potatohead2.png";
         wrapper.dataset.index = "2";
@@ -35,6 +37,27 @@ class Game {
         wrapper.dataset.index = "1";
       }
       messageContainer.innerText = "";
-    })
+    });
   }
 }
+
+// navigation.js
+// const navBar = () => {
+//   // create navbar element as a container
+// };
+
+// const createNavElements = () => {
+//   // elements that go in navbar
+// };
+
+// export default navBar
+
+// // utilities.js
+
+// const createId = () => {
+
+// }
+
+// export const createClassNames = (classNamesArray, element) => {
+//   // iterates through class names array and creates a class for some element
+// }
